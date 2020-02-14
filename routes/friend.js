173245e2e0ddf114eaf21560ab697f00bebd17b2
jil1064 <49259714+jil1064@users.noreply.github.com>
2020-02-13 19:01:15 -0800

    found friend

--- routes/friend.js
+++ routes/friend.js
@@ -1,4 +1,5 @@
 var data = require("../data/friends.json");
+var total_events = require("../data/total.json");
 
 exports.list = function(req, res) {
   var friend = req.params.friend;
@@ -10,6 +11,16 @@ exports.list = function(req, res) {
     if (friends_arr[i].name === friend) {
       found = friends_arr[i];
     }
+  }
+
+  // if found the friend, push all activities of friends
+  // into a new array to be rendered
+  if (found) {
+    var activites = total_events.events.filter(
+      event => event.weeth === found.name
+    );
+    found.activities = [];
+    found.activities.push(...activites);
   }
   console.log(found);
   res.render("friend", found);
